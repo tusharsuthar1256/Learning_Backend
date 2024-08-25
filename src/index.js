4cfbafb0daedd5f1ecfa07510569713ef9678a41
tusharsuthar1256 <153266231+tusharsuthar1256@users.noreply.github.com>
2024-08-25 18:15:40 +0530

    Add refresh Token

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
 dotenv.config({
-     path: "./env"
+     path: "./.env"
 });
 
 
@@ -24,7 +24,6 @@ connectDB()
      console.log("MongoDB connect failed : ",err);
      
 })
-
 
 
 // import express from "expresss";
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs';
+
+cloudinary.config({
+     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+     api_key: process.env.CLOUDINARY_API_KEY, 
+     api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadOnCloudinary = async (localFilePath) => {
+     try {
+          if (!localFilePath) return null
+          //Upload the file on cloudinary
+          const response = await cloudinary.uploader.upload(localFilePath,{
+               resource_type:"auto"
+          })
+          //File has been uploaded successful
+          // console.log("File is uploaded on cloudnary",response.url);
+          fs.unlinkSync(localFilePath);
+          return response
+     } catch (error) {
+          fs.unlinkSync(localFilePath) //remove the locally file as the upload operation got failed
+          return null;
+     }
+}
+
+export {uploadOnCloudinary}
+
+
