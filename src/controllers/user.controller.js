e83034fa1b6c41715332348216d84c86e4bd0ece
tusharsuthar1256 <tusharsuthar081@gmail.com>
2026-04-08 10:20:50 +0530

    minor issues

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -142,6 +142,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    sameSite: "none",
   };
 
   return res
@@ -176,6 +177,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    sameSite: "none",
   };
 
   return res
@@ -212,6 +214,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     const options = {
       httpOnly: true,
       secure: true,
+      sameSite: "none",
     };
 
     const { accessToken, newRefreshToken } =
